979be02e05c026518ec6e042a0cc8ed440b8c763
Kobby64 <72305112+Kobby64@users.noreply.github.com>
2020-10-03 09:07:35 -0400

    Add files via upload

---
+++ PYHW16.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 16:19:00 2020
+
+@author: ofori
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 16:32:52 2020
+
+@author: ofori
+"""
+
+#numpy package to calculate log
+import numpy as np
+
+# add function for addition
+def add(x, y):
+ return x + y
+
+# subtract function used for subtraction
+def subtract(x, y):
+ return x - y
+
+# multiply function used for multiplication
+def multiply(x, y):
+ return x * y
+
+# divide function used for divide the two numbers
+def divide(x, y):
+ return x / y
+# for natural log
+def naturalLogarithm(x):
+ return np.log(x)
+# for base10 log
+
+def base10Logarithm(x):
+ return np.log10(x)
+# for base2 log
+
+def base2Logarith(x, y):
+ return np.log2(x)
